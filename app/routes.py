0b27bd5a16d0b2261af3cc692a88a5c6da1ae1f7
Matias Cardone <cardone.matias@hotmail.com>
2020-05-03 20:41:59 +0200

    Added content to aboutcovid.html.css

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,6 @@
 from flask import render_template, url_for
 from app import app
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
 @app.route('/')
 @app.route('/index')
 def index():
